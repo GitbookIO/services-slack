64dc300bd9f625f354daf8e241515da2c36e5a77
Samy Pessé <samypesse@gmail.com>
2016-01-29 14:35:11 +0100

    Add support for events 'thread' and 'thread_comment'

--- index.js
+++ index.js
@@ -6,26 +6,46 @@ var GitBookMiddleware = require('gitbook-webhook-middleware');
 var app = express();
 var gitbookMiddleware = GitBookMiddleware();
 
+var EVENTS = {
+    'publish': function(payload) {
+        var book = payload.book;
+        var build = payload.build;
+        return '<'+build.author.urls.profile+'|'+build.author.name+'> published a new update of <'+book.urls.access+'|'+book.title+'>';
+    },
+    'thread': function(payload) {
+        var book = payload.book;
+        var thread = payload.thread;
+        return 'Discussion <'+thread.urls.details+'|#'+thread.number+'> "'+thread.title+'" has been ' + payload.action;
+    },
+    'thread_comment': function(payload) {
+        var book = payload.book;
+        var thread = payload.thread;
+        var comment = payload.comment;
+        return '<'+comment.user.urls.profile+'|'+comment.user.name+'>" commented on discussion <'+thread.urls.details+'|#'+thread.number+'> "'+thread.title+'"';
+    }
+};
+
+
 app.get('/', function (req, res) {
     res.redirect('https://github.com/GitbookIO/services-slack');
 });
 
 app.post('/hook/v1/:tokens([\\/.\\w]*|)', gitbookMiddleware, function (req, res, next) {
-    if (req.headers['x-gitbook-event'] !== 'publish') {
+    var messageBuilder = EVENTS[req.headers['x-gitbook-event']];
+
+    if (!messageBuilder) {
         return res.status(200).end();
     }
 
     // Extract payload
     var payload = req.body.payload;
-    var book = payload.book;
-    var build = payload.build;
 
     // Build slack client
     var hookUrl = url.resolve('https://hooks.slack.com/services/', req.params.tokens)
     var slack = new Slack(hookUrl);
 
     slack.send({
-        text: '<'+build.author.urls.profile+'|'+build.author.name+'> published a new update of <'+book.urls.access+'|'+book.title+'>',
+        text: messageBuilder(payload),
         username: 'GitBook',
         icon_url: 'https://www.gitbook.com/assets/images/logo/128.png'
     }, function(err, body) {
